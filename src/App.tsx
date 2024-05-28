2812709776fa8c559f061addb3f3aca3094437eb
maci-kb24 <macikb8@yahoo.com>
2024-05-28 16:27:56 +0200

    create the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
+import Header from './components/Header'
 function App() {
 
   return (
     <div className="w-full h-full flex flex-col relative box-border">
-    <h1>Hello</h1>
-      
+      <Header />
     </div>
   )
 }

8cf0f80e193a59cf47f327057433c48bfcd95819
maci-kb24 <macikb8@yahoo.com>
2024-05-28 16:48:33 +0200

    create the blog component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Blog from './components/Blog'
 import Header from './components/Header'
+import Services from './components/Services'
 function App() {
 
   return (
     <div className="w-full h-full flex flex-col relative box-border">
       <Header />
+      <Services />
+      <Blog />
     </div>
   )
 }
